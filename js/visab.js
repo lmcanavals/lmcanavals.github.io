c412a1458ae81f3482a81dcdff61864e4ea9052a
Luis Martín Canaval Sánchez <public01@canaval.info>
2016-07-06 06:01:05 -0500

    second fp commit

--- js/visab.js
+++ js/visab.js
@@ -2,56 +2,98 @@
 
   d3.select('body').style('width', width + 'px');
 
-  var datafile = 'data/tsevent.txt.tsv';
-  var width = 1280,
-  height = width * 9 / 16;
+  var datafileTsu = 'data/tsevent.txt.tsv';
+  var datafileVol = 'data/volerup.txt.tsv';
+  var width = 1280, height = width * 9 / 16;
+  var graticule = d3.geo.graticule();
+  var λ = d3.scale.linear().domain([0, width]).range([-180, 180]);
+  var φ = d3.scale.linear().domain([0, height]).range([90, -90]);
+  var scaler = d3.scale.linear().range([2, 30]);
+  var scalec = d3.scale.linear().range([0, 255]);
+  var pcur = [width / 2, height / 2];
+  var pmousei;
+  var move = false;
+  var ftsux = 'LONGITUDE';
+  var ftsuy = 'LATITUDE';
+  var ftsur = 'FOCAL_DEPTH';
+  var ftsuc = 'SOLOVIEV';
+  var ftsuf = 'YEAR';
+  var fvolx = 'Longitude';
+  var fvoly = 'Latitude';
+  var fvolr = 'Elevation';
+  var fvolc = 'VEI';
+  var fvolf = 'Year';
+  var ymin = -4360;
+  var ymax = 2020;
+
   var svg = d3.select('body').append('svg')
     .attr('width', width)
     .attr('height', height);
   var gMap = svg.append('g');
   var gData = svg.append('g');
-
-  var vproy = ['Ortographic', 'Kavrayskiy VII', 'Natural Earth'];
-
-  var see = d3.select('body').append('div').append('select');
-  see.selectAll('option').data(vproy).enter().append('option').attr('value', function(d){return d;});
-
-  var projection1 = d3.geo.orthographic()
+  var vproj = [
+  {'id': 0, 'name': 'Orthographic', 'projection': d3.geo.orthographic()
     .scale(height * .5)
     .translate([width / 2, height / 2])
-    .clipAngle(90);
-
-  var projection2 = d3.geo.kavrayskiy7()
+    .clipAngle(90)},
+  {'id': 1, 'name': 'Kavrayskiy VII', 'projection': d3.geo.kavrayskiy7()
     .scale(height * .315)
-    .translate([width / 2, height / 2]);
-
-  var projection = d3.geo.naturalEarth()
+    .translate([width / 2, height / 2])},
+  {'id': 2, 'name': 'Natural Earth', 'projection': d3.geo.naturalEarth()
     .scale(height * .315)
-    .translate([width / 2, height / 2]);
+    .translate([width / 2, height / 2])}];
+  var projection;
+  var path = d3.geo.path();
 
-  var path = d3.geo.path()
-    .projection(projection);
+  var see = d3.select('body').append('div').append('select');
+  see.selectAll('option').data(vproj).enter()
+    .append('option')
+    .attr('value', function(d) { return d.id; })
+    .text(function(d) { return d.name; });
+  see.on('click', function() {
+    changeProjection(this.selectedOptions[0].value);
+    updateStuff();
+    pcur = [width / 2, height / 2];
+  });
 
-  var graticule = d3.geo.graticule();
 
-  var λ = d3.scale.linear().domain([0, width]).range([-180, 180]);
-  var φ = d3.scale.linear().domain([0, height]).range([90, -90]);
-  var scalex = d3.scale.linear().range([0, width]);
-  var scaley = d3.scale.linear().range([height, 0]);
-  var scaler = d3.scale.linear().range([3, 100]);
-  var scalec = d3.scale.linear().range([50, 255]);
-  var fieldx = 'LONGITUDE';
-  var fieldy = 'LATITUDE';
-  var fieldr = 'FOCAL_DEPTH';
-  var fieldc = 'MAXIMUM_WATER_HEIGHT';
-
-  svg.on('mousemove', function() {
-    var p = d3.mouse(this);
-    projection.rotate([λ(p[0]), φ(p[1])]);
+  function updateStuff() {
     svg.selectAll('path').attr('d', path);
     svg.selectAll('circle')
-      .attr('cx', function(d) { return projection([d[fieldx], d[fieldy]])[0]; })
-      .attr('cy', function(d) { return projection([d[fieldx], d[fieldy]])[1]; })
+      .attr('cx', function(d) { return projection([d[ftsux], d[ftsuy]])[0]; })
+      .attr('cy', function(d) { return projection([d[ftsux], d[ftsuy]])[1]; })
+      .attr('visibility', function(d) {
+        return (d[ftsuf] >= ymin && d[ftsuf] <= ymax)? 'visible': 'hidden';
+      });
+    svg.selectAll('rect')
+      .attr('x', function(d) { return projection([d[fvolx], d[fvoly]])[0]; })
+      .attr('y', function(d) { return projection([d[fvolx], d[fvoly]])[1]; })
+      .attr('visibility', function(d) {
+        return (d[fvolf] >= ymin && d[fvolf] <= ymax)? 'visible': 'hidden';
+      });
+  }
+
+  function changeProjection(i) {
+    projection = vproj[i].projection;
+    path.projection(projection);
+  }
+
+  changeProjection(0);
+  svg.on('mousedown', function() {
+    pmousei = d3.mouse(this);
+    move = true;
+  });
+  svg.on('mouseup', function() {
+    move = false;
+    var p = d3.mouse(this);
+    pcur = [pcur[0] + p[0] - pmousei[0], pcur[1] + p[1] - pmousei[1]];
+  });
+  svg.on('mousemove', function() {
+    if (!move) return false;
+    var p = d3.mouse(this);
+    ptemp = [pcur[0] + p[0] - pmousei[0], pcur[1] + p[1] - pmousei[1]];
+    projection.rotate([λ(ptemp[0]), φ(ptemp[1])]);
+    updateStuff();
   });
 
   gMap.append('path')
@@ -72,7 +114,8 @@
       .attr('d', path);
 
     gMap.insert('path', '.graticule')
-      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+      .datum(topojson.mesh(world, world.objects.countries,
+            function(a, b) { return a !== b; }))
       .attr('class', 'boundary')
       .attr('d', path);
   });
@@ -86,24 +129,51 @@
     return d;
   };
 
-  function dataProcessing(error, data) {
+  function dataProcessingTsu(error, data) {
     if (error) throw error;
-    scalex.domain(d3.extent(data, function(d) { return d[fieldx]; }));
-    scaley.domain(d3.extent(data, function(d) { return d[fieldy]; }));
-    scaler.domain(d3.extent(data, function(d) { return d[fieldr]; }));
-    scalec.domain(d3.extent(data, function(d) { return d[fieldc]; }));
-    if (error) throw error;
-    var circles = gData.selectAll('circle').data(data)
-      circles.enter().append('circle')
-      .attr('cx', function(d) { return projection([d[fieldx], d[fieldy]])[0]; })
-      .attr('cy', function(d) { return projection([d[fieldx], d[fieldy]])[1]; })
-      .attr('r', function(d) { return scaler(d[fieldr]); })
-      .style('opaque', 0.2)
-      .style('fill', function(d) { return 'rgb(0, 0, '+Math.floor(scalec(d[fieldc]))+')'; });
+    scaler.domain(d3.extent(data, function(d) { return d[ftsur]; }));
+    scalec.domain(d3.extent(data, function(d) { return d[ftsuc]; }));
+    var circles = gData.selectAll('circle').data(data);
+    circles.enter().append('circle')
+      .attr('cx', function(d) { return projection([d[ftsux], d[ftsuy]])[0]; })
+      .attr('cy', function(d) { return projection([d[ftsux], d[ftsuy]])[1]; })
+      .attr('r', function(d) { return scaler(d[ftsur]); })
+      .style('fill', function(d) {
+        var rb = Math.floor(scalec(d[ftsuc]));
+        return 'rgb(' + rb + ', ' + rb + ', 255)';
+      });
     circles.exit().remove();
   }
 
-  d3.tsv(datafile, doTypesTsu, dataProcessing);
+  function doTypesVol(d) {
+    for (attrName in d) {
+      if (attrTypeVol[attrName].type !== NONE) {
+        d[attrName] = convert(d[attrName], attrTypeVol[attrName]);
+      }
+    }
+    return d;
+  };
+
+  function dataProcessingVol(error, data) {
+    if (error) throw error;
+    scaler.domain(d3.extent(data, function(d) { return d[fvolr]; }));
+    scalec.domain(d3.extent(data, function(d) { return d[fvolc]; }));
+    var rects = gData.selectAll('rect').data(data);
+    rects.enter().append('rect')
+      .attr('x', function(d) { return projection([d[fvolx], d[fvoly]])[0]; })
+      .attr('y', function(d) { return projection([d[fvolx], d[fvoly]])[1]; })
+      .attr('width', function(d) { return scaler(d[fvolr]); })
+      .attr('height', function(d) { return scaler(d[fvolr]); })
+      .style('fill', function(d) {
+        var rb = Math.floor(scalec(d[fvolc]));
+        return 'rgb(255, ' + rb + ', ' + rb + ')';
+      });
+    rects.exit().remove();
+    updateTsuStuff();
+  }
+
+  d3.tsv(datafileTsu, doTypesTsu, dataProcessingTsu);
+  d3.tsv(datafileVol, doTypesVol, dataProcessingVol);
 
   d3.select(self.frameElement).style('height', height + 'px');
 
