9062be73fc4b13c55daef9e917101e1c27c4e31a
Luis M Canaval S <43156214+lmcanavals@users.noreply.github.com>
2020-12-04 01:15:53 -0500

    Delete lmcs.js

--- js/lmcs.js
+++
@@ -1,9 +0,0 @@
-const u = c => String.fromCodePoint(c);
-console.log(`***
-+--------------------------------------------------------------+
-| Hello, I'm Luis Martín Canaval Sánchez from Huamachuco, Perú |
-| Find me at:                                                  |
-| {github.com,twitter.com,instagram.com}/lmcanavals            |
-+--------------------------------------------------------------+
-Here, have some emojis:
-${u(128058)} ${u(129440)} ${u( 128126)} ${u(129302)} ${u(128125)}`);
